d74c8d700a4d4b783c6ddfb369ba832346837433
yangyang0615 <tianyang903@163.com>
2020-03-30 00:03:57 +0800

    添加

--- src/com/etoak/pojo/User.java
+++ src/com/etoak/pojo/User.java
@@ -9,5 +9,45 @@ public class User {
     private String password;
     private Integer age;
     private String email;
-    private String ;
+    private String order;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
 }
